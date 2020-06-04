43cda9e0bf08ed19c96222d8001e344eb84dfefe
Hassene <hassene.ayoub@yahoo.fr>
2020-06-04 03:07:33 +0100

    V3.9.8

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/Contacter", require("./routes/api/Contacter"));
 //serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
